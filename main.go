c4f8986041bbdecf7bb380a98d66a4591d58d2a3
João Victor da Silva <jv782063@gmail.com>
2024-11-05 17:36:46 -0300

    finalizes the algorithm with 3 tests to make sure it's working

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type (
 	Gene struct {
@@ -8,29 +13,135 @@ type (
 	}
 
 	Chromosome struct {
-		Genes []*Gene
+		Genes []Gene
+	}
+
+	PMX struct {
+		Parent1, Parent2 *Chromosome
+		StartPoint       int
+		EndPoint         int
+		Size             int
 	}
 )
 
-func NewGene(value int) *Gene {
-	return &Gene{value}
+func NewGene(value int) Gene {
+	return Gene{value}
 }
 
-func NewChromosome(genes []*Gene) *Chromosome {
+func NewChromosome(genes []Gene) *Chromosome {
 	return &Chromosome{genes}
 }
 
-func PMX(parent1, parent2 *Chromosome) [2]*Chromosome {
+func NewPMX(parent1, parent2 *Chromosome) *PMX {
+	chromosomeSize := len(parent1.Genes)
 	if len(parent1.Genes) != len(parent2.Genes) {
 		panic(errors.New("The chromosomes must contain the same number of genes"))
 	}
 
-	child1 := new(Chromosome)
-	child2 := new(Chromosome)
-	children := [2]*Chromosome{child1, child2}
+	startPoint, endPoint := GetCrossoverPoints(chromosomeSize)
+	return &PMX{parent1, parent2, startPoint, endPoint, chromosomeSize}
+}
 
+func (pmx *PMX) Run() [2]*Chromosome {
+	child1Genes := make([]Gene, pmx.Size)
+	child2Genes := make([]Gene, pmx.Size)
+	for i := pmx.StartPoint; i <= pmx.EndPoint; i++ {
+		child1Genes[i] = pmx.Parent2.Genes[i]
+		child2Genes[i] = pmx.Parent1.Genes[i]
+	}
+
+	fillRemainingGenes(child1Genes, pmx.Parent1.Genes, pmx.StartPoint, pmx.EndPoint)
+	fillRemainingGenes(child2Genes, pmx.Parent2.Genes, pmx.StartPoint, pmx.EndPoint)
+
+	children := [2]*Chromosome{NewChromosome(child1Genes), NewChromosome(child2Genes)}
 	return children
 }
 
+func fillRemainingGenes(childGenes, parentGenes []Gene, startPoint, endPoint int) {
+	chromosomeSize := len(parentGenes)
+	for i := range chromosomeSize {
+		if i >= startPoint && i <= endPoint {
+			continue
+		}
+
+		gene := parentGenes[i]
+		for contains(childGenes, gene) {
+			gene = parentGenes[indexOfGene(childGenes, gene)]
+		}
+		childGenes[i] = NewGene(gene.Value)
+	}
+}
+
+func contains(genes []Gene, gene Gene) bool {
+	for _, g := range genes {
+		if g.Value == gene.Value {
+			return true
+		}
+	}
+	return false
+}
+
+func indexOfGene(genes []Gene, gene Gene) int {
+	for i, g := range genes {
+		if g.Value == gene.Value {
+			return i
+		}
+	}
+	return -1
+}
+
+func GetCrossoverPoints(size int) (startPoint, endPoint int) {
+	randSource := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(randSource)
+	startPoint = rnd.Intn(size)
+	endPoint = rnd.Intn(size - 1)
+	if endPoint >= startPoint {
+		endPoint++
+	} else {
+		startPoint, endPoint = endPoint, startPoint
+	}
+	return
+}
+
 func main() {
+	parent1 := NewChromosome([]Gene{
+		NewGene(1),
+		NewGene(2),
+		NewGene(3),
+		NewGene(4),
+		NewGene(5),
+		NewGene(6),
+		NewGene(7),
+		NewGene(8),
+		NewGene(9),
+	})
+	parent2 := NewChromosome([]Gene{
+		NewGene(4),
+		NewGene(5),
+		NewGene(6),
+		NewGene(9),
+		NewGene(8),
+		NewGene(1),
+		NewGene(3),
+		NewGene(2),
+		NewGene(7),
+	})
+	pmx := NewPMX(parent1, parent2)
+	children := pmx.Run()
+	fmt.Println("Crossover Points =>", "Start:", pmx.StartPoint, "End:", pmx.EndPoint)
+	fmt.Println("Parents:")
+	printGenes(parent1.Genes)
+	printGenes(parent2.Genes)
+	fmt.Println("\nChildren:")
+	for _, child := range children {
+		printGenes(child.Genes)
+	}
+}
+
+func printGenes(g []Gene) {
+	values := make([]int, len(g))
+	for i, gene := range g {
+		values[i] = gene.Value
+	}
+	fmt.Println(fmt.Sprint(values))
 }
--- main_test.go
+++ main_test.go
@@ -8,12 +8,12 @@ import (
 
 func TestPMX(t *testing.T) {
 	t.Run("should return different chromosome size error", func(t *testing.T) {
-		parent1Genes := []*Gene{
+		parent1Genes := []Gene{
 			NewGene(1),
 			NewGene(2),
 			NewGene(3),
 		}
-		parent2Genes := []*Gene{
+		parent2Genes := []Gene{
 			NewGene(2),
 			NewGene(3),
 		}
@@ -21,62 +21,191 @@ func TestPMX(t *testing.T) {
 		parent2 := NewChromosome(parent2Genes)
 
 		assert.PanicsWithError(t, "The chromosomes must contain the same number of genes", func() {
-			PMX(parent1, parent2)
+			NewPMX(parent1, parent2)
 		})
 	})
 
 	t.Run("should return the crossed children with the correct values", func(t *testing.T) {
-		parent1Genes := []*Gene{
-			NewGene(1),
-			NewGene(2),
-			NewGene(3),
-			NewGene(4),
-			NewGene(5),
-			NewGene(6),
-			NewGene(7),
-			NewGene(8),
-			NewGene(9),
-		}
-		parent2Genes := []*Gene{
-			NewGene(5),
-			NewGene(4),
-			NewGene(6),
-			NewGene(9),
-			NewGene(2),
-			NewGene(1),
-			NewGene(7),
-			NewGene(8),
-			NewGene(3),
-		}
-		parent1 := NewChromosome(parent1Genes)
-		parent2 := NewChromosome(parent2Genes)
+		t.Run("1", func(t *testing.T) {
+			parent1Genes := []Gene{
+				NewGene(1),
+				NewGene(2),
+				NewGene(3),
+				NewGene(4),
+				NewGene(5),
+				NewGene(6),
+				NewGene(7),
+				NewGene(8),
+				NewGene(9),
+			}
+			parent2Genes := []Gene{
+				NewGene(5),
+				NewGene(4),
+				NewGene(6),
+				NewGene(9),
+				NewGene(2),
+				NewGene(1),
+				NewGene(7),
+				NewGene(8),
+				NewGene(3),
+			}
+			parent1 := NewChromosome(parent1Genes)
+			parent2 := NewChromosome(parent2Genes)
+			pmx := NewPMX(parent1, parent2)
+			// mock
+			pmx.StartPoint, pmx.EndPoint = 2, 5
 
-		children := PMX(parent1, parent2)
-		child1 := children[0]
-		child2 := children[1]
+			children := pmx.Run()
+			child1 := children[0]
+			child2 := children[1]
 
-		t.Run("child1 values", func(t *testing.T) {
-			assert.Equal(t, 1, child1.Genes[0].Value)
-			assert.Equal(t, 5, child1.Genes[1].Value)
-			assert.Equal(t, 6, child1.Genes[2].Value)
-			assert.Equal(t, 9, child1.Genes[3].Value)
-			assert.Equal(t, 2, child1.Genes[4].Value)
-			assert.Equal(t, 1, child1.Genes[5].Value)
-			assert.Equal(t, 7, child1.Genes[6].Value)
-			assert.Equal(t, 8, child1.Genes[7].Value)
-			assert.Equal(t, 4, child1.Genes[8].Value)
+			t.Run("child1 values", func(t *testing.T) {
+				assert.Equal(t, 3, child1.Genes[0].Value)
+				assert.Equal(t, 5, child1.Genes[1].Value)
+				assert.Equal(t, 6, child1.Genes[2].Value)
+				assert.Equal(t, 9, child1.Genes[3].Value)
+				assert.Equal(t, 2, child1.Genes[4].Value)
+				assert.Equal(t, 1, child1.Genes[5].Value)
+				assert.Equal(t, 7, child1.Genes[6].Value)
+				assert.Equal(t, 8, child1.Genes[7].Value)
+				assert.Equal(t, 4, child1.Genes[8].Value)
+			})
+
+			t.Run("child2 values", func(t *testing.T) {
+				assert.Equal(t, 2, child2.Genes[0].Value)
+				assert.Equal(t, 9, child2.Genes[1].Value)
+				assert.Equal(t, 3, child2.Genes[2].Value)
+				assert.Equal(t, 4, child2.Genes[3].Value)
+				assert.Equal(t, 5, child2.Genes[4].Value)
+				assert.Equal(t, 6, child2.Genes[5].Value)
+				assert.Equal(t, 7, child2.Genes[6].Value)
+				assert.Equal(t, 8, child2.Genes[7].Value)
+				assert.Equal(t, 1, child2.Genes[8].Value)
+			})
 		})
 
-		t.Run("child2 values", func(t *testing.T) {
-			assert.Equal(t, 2, child2.Genes[0].Value)
-			assert.Equal(t, 9, child2.Genes[1].Value)
-			assert.Equal(t, 3, child2.Genes[2].Value)
-			assert.Equal(t, 4, child2.Genes[3].Value)
-			assert.Equal(t, 5, child2.Genes[4].Value)
-			assert.Equal(t, 6, child2.Genes[5].Value)
-			assert.Equal(t, 7, child2.Genes[6].Value)
-			assert.Equal(t, 8, child2.Genes[7].Value)
-			assert.Equal(t, 1, child2.Genes[8].Value)
+		t.Run("2", func(t *testing.T) {
+			parent1Genes := []Gene{
+				NewGene(9),
+				NewGene(2),
+				NewGene(7),
+				NewGene(5),
+				NewGene(4),
+				NewGene(3),
+				NewGene(6),
+				NewGene(1),
+				NewGene(8),
+			}
+			parent2Genes := []Gene{
+				NewGene(2),
+				NewGene(8),
+				NewGene(3),
+				NewGene(6),
+				NewGene(9),
+				NewGene(5),
+				NewGene(7),
+				NewGene(4),
+				NewGene(1),
+			}
+			parent1 := NewChromosome(parent1Genes)
+			parent2 := NewChromosome(parent2Genes)
+			pmx := NewPMX(parent1, parent2)
+			// mock
+			pmx.StartPoint, pmx.EndPoint = 3, 5
+
+			children := pmx.Run()
+			child1 := children[0]
+			child2 := children[1]
+
+			t.Run("child1 values", func(t *testing.T) {
+				assert.Equal(t, 4, child1.Genes[0].Value)
+				assert.Equal(t, 2, child1.Genes[1].Value)
+				assert.Equal(t, 7, child1.Genes[2].Value)
+				assert.Equal(t, 6, child1.Genes[3].Value)
+				assert.Equal(t, 9, child1.Genes[4].Value)
+				assert.Equal(t, 5, child1.Genes[5].Value)
+				assert.Equal(t, 3, child1.Genes[6].Value)
+				assert.Equal(t, 1, child1.Genes[7].Value)
+				assert.Equal(t, 8, child1.Genes[8].Value)
+			})
+
+			t.Run("child2 values", func(t *testing.T) {
+				assert.Equal(t, 2, child2.Genes[0].Value)
+				assert.Equal(t, 8, child2.Genes[1].Value)
+				assert.Equal(t, 6, child2.Genes[2].Value)
+				assert.Equal(t, 5, child2.Genes[3].Value)
+				assert.Equal(t, 4, child2.Genes[4].Value)
+				assert.Equal(t, 3, child2.Genes[5].Value)
+				assert.Equal(t, 7, child2.Genes[6].Value)
+				assert.Equal(t, 9, child2.Genes[7].Value)
+				assert.Equal(t, 1, child2.Genes[8].Value)
+			})
+		})
+
+		t.Run("3", func(t *testing.T) {
+			parent1Genes := []Gene{
+				NewGene(1),
+				NewGene(2),
+				NewGene(3),
+				NewGene(4),
+			}
+			parent2Genes := []Gene{
+				NewGene(2),
+				NewGene(3),
+				NewGene(1),
+				NewGene(4),
+			}
+			parent1 := NewChromosome(parent1Genes)
+			parent2 := NewChromosome(parent2Genes)
+			pmx := NewPMX(parent1, parent2)
+			// mock
+			pmx.StartPoint, pmx.EndPoint = 0, 1
+
+			children := pmx.Run()
+			child1 := children[0]
+			child2 := children[1]
+
+			t.Run("child1 values", func(t *testing.T) {
+				assert.Equal(t, 2, child1.Genes[0].Value)
+				assert.Equal(t, 3, child1.Genes[1].Value)
+				assert.Equal(t, 1, child1.Genes[2].Value)
+				assert.Equal(t, 4, child1.Genes[3].Value)
+			})
+
+			t.Run("child2 values", func(t *testing.T) {
+				assert.Equal(t, 1, child2.Genes[0].Value)
+				assert.Equal(t, 2, child2.Genes[1].Value)
+				assert.Equal(t, 3, child2.Genes[2].Value)
+				assert.Equal(t, 4, child2.Genes[3].Value)
+			})
 		})
 	})
+}
+
+func TestContains(t *testing.T) {
+	genes := []Gene{
+		NewGene(1),
+		NewGene(4),
+		NewGene(16),
+		NewGene(28),
+		NewGene(10),
+	}
+	existsGene := NewGene(28)
+	noExistsGene := NewGene(282)
+
+	assert.True(t, contains(genes, existsGene))
+	assert.False(t, contains(genes, noExistsGene))
+}
+
+func TestIndexOfGene(t *testing.T) {
+	genes := []Gene{
+		NewGene(1),
+		NewGene(4),
+		NewGene(16),
+		NewGene(28),
+		NewGene(10),
+	}
+
+	assert.Equal(t, 3, indexOfGene(genes, NewGene(28)))
+	assert.Equal(t, 1, indexOfGene(genes, NewGene(4)))
 }
